50710bdefe0ec7eb35c4c93534a620fdabecaefe
Arturo <arturo.diaz@wavesoluciones.com>
2013-03-18 00:46:16 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inviertelocal::Application.config.secret_token = 'ae88354621b8b1a30745ddc6b81034b9cdf031b2a846db4caf5edf83b74b7d4b7adb920f1f89068a73a04e36f57c9f38917c9c017659297dd8d5ba5456ac2b69'
